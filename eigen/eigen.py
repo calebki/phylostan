989c8d941d08cc5b320cfc5db01a7f958e368d5a
Caleb Ki <calebki94@gmail.com>
2020-08-03 08:47:09 -0400

    minor bug fixes

--- eigen/eigen.py
+++ eigen/eigen.py
@@ -14,13 +14,13 @@ assert all(len(v) == NUM_SITES for v in tip_data.values())
 G = Phylo.to_networkx(Phylo.read(tree_file, 'newick', rooted=True))
 # FIXME this just arbitrarily assigns the leaves to the first n nodes in some
 # way. Need to make sure it matches up with tip_data.
-print(G.nodes)
+print(G.nodes())
 n = len(tip_data)
 leaves = iter(range(n))
 interior = iter(range(n, 2 * n - 1))
 node_remap = {}
 tip_remap = {}
-for c in G.nodes():
+for c in nx.dfs_postorder_nodes(G):
     if c.name is not None:
         node_id = next(leaves)
         node_remap[c] = node_id
@@ -29,6 +29,11 @@ for c in G.nodes():
         node_remap[c] = next(interior)
 G = nx.relabel_nodes(G, mapping=node_remap)
 postorder = list(nx.dfs_postorder_nodes(G))
+preorder_map = np.empty((len(G),2), dtype=int)
+preorder_map[:,0] = list(nx.dfs_preorder_nodes(G))
+for i in range(len(G)):
+    preorder_map[i,1] = list(G.pred[preorder_map[i,0]])[0] if list(G.pred[preorder_map[i,0]]) else 0
+preorder_map += 1
 child_parent = [None] * len(G) # the i-th entry of child_parent is the parent of node i
 for k in G.pred:
     child_parent[k] = list(G.pred[k])[0] if G.pred[k] else -1
@@ -56,7 +61,7 @@ with open("eigen.hpp", "wt") as out:
     out.write(s)
 
 # encode partials
-data = {'S':n, 'L':NUM_SITES, 'map':child_parent, 'rate':1.0, 'lower_root':0.0}
+data = {'S':n, 'L':NUM_SITES, 'map':preorder_map, 'rate':1.0, 'lower_root':0.0}
 seed = 1
 
 include_files = ["eigen.hpp"]
@@ -69,4 +74,4 @@ sm = pystan.StanModel(file="example.stan",
                       extra_compile_args=["--std=c++14", "-Wno-int-in-bool-context"]  # "-Wfatal-errors", 
                       )
 fit = sm.vb(data=data, iter=1000, algorithm='meanfield', seed = seed)
-print(fit['mean_pars'])
+print(fit['mean_pars'][fit['mean_par_names']=='theta'])
