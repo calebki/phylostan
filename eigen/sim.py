8378a738177d0a238f248adb98c050f911639dbe
Caleb Ki <calebki94@gmail.com>
2020-10-06 19:58:24 -0400

    minor improvements to compile time

--- eigen/sim.py
+++ eigen/sim.py
@@ -1,6 +1,10 @@
 from util import make_header, run_stan
+import time
     
 data = make_header('sim.tree', 'sim.fa', 'fasta', 'dates.csv', 'sim.hpp')
 data['m'] = 10
 data['x1'] = 0
+
+start = time.time()
 fit = run_stan('sim.hpp', 'vbsky_fix_rate.stan', data, 1, 'sim')
+print(time.time()-start)
